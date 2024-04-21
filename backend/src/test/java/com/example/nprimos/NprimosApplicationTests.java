09ba6350a69c4dbace8ea097f223f5393c3b8592
felipeverones <felipe_verones@hotmail.com>
2024-04-21 03:07:36 -0300

    Reestruturado projeto com subdiretório backend

---
+++ backend/src/test/java/com/example/nprimos/NprimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nprimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NprimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
